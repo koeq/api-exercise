c8bf1f5d8905b48b323b86d26b64bc6a3ef9ed05
koebe1 <koe.be.pay@web.de>
2021-08-25 15:39:37 +0200

    fetched data from an api for exercise purposes

---
+++ api.js
@@ -0,0 +1,65 @@
+// leanring to fetch data from an api
+
+let apiList = document.getElementById("api-list");
+
+// create error handler
+function handleError(err) {
+  console.log(`${err}`);
+  // display error instead of api content
+  createApiElement("li");
+  newElement.style.color = "orange";
+  newElement.textContent = err;
+}
+
+// helper function to create a new element
+function createApiElement(element) {
+  apiList.appendChild(document.createElement(element));
+  newElement = document.getElementsByTagName(element)[0];
+}
+
+const endpoint = "https://randomuser.me/api/";
+
+// // OPTION 1
+// // displaying data from api using .then()-syntax
+
+// // api url
+
+// // fetch data from api
+// const userPromise = fetch(endpoint);
+
+// userPromise
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     // picture
+//     const picture = data.results[0].picture.large;
+//     createApiElement("img");
+//     newElement.src = picture;
+
+//     // email
+//     const email = data.results[0].email;
+//     createApiElement("li");
+
+//     newElement.textContent = `-> user email: ${email}`;
+//   })
+//   .catch(handleError);
+
+// OPTION 2 -> way to go
+// displaying data from api using async await
+async function displayUser() {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  const picture = data.results[0].picture.large;
+
+  // image
+  createApiElement("img");
+  newElement.src = picture;
+
+  // email
+  const email = data.results[0].email;
+  createApiElement("li");
+  newElement.textContent = `-> user email: ${email}`;
+}
+
+window.addEventListener("DOMContentLoaded", displayUser().catch(handleError));
